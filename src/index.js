7a9522f57e60080bcc4880dbacd8ce7eb1868138
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-29 16:31:44 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import $ from 'jquery';
+import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import PetSearch from "./js/search.js";
 
+function getElements(resp) {
+    console.log(resp);
+}
+
 $(document).ready(function () {
   $("#zipCode").submit(function (event) {
     event.preventDefault();
-    let breed = $("#dogs").val();
-    PetSearch.getPet(breed);
+    let breedName = $("#dogs").val();
+    let postalCode = $("#zip").val();
+    // let test = new PetSearch();
+    // console.log(PetSearch.getPet(breedName,postalCode));
+    // PetSearch.getPet(breedName, postalCode).then(function (animals) {
+    //   console.log("i am working");
+    //   returnOutput(animals);
+    (async function () {
+      const resp = await PetSearch.getPet(breedName, postalCode);
+      getElements(resp);
+    })();
   });
 });
+
+// async function makeApiCall(breedName,postalCode){
+//   const response = await PetSearch.getPet(breedName, postalCode);
+//   console.log(response);
+//   // returnOutput(response);
+// }
+
+// makeApiCall(breedName,postalCode);
+
+// function returnOutput(animals) {
+//   $(".output").html(`<p>${animals}</p>`);
+// }
--- src/js/search.js
+++ src/js/search.js
@@ -1,11 +1,11 @@
-// import authToken from "./token.js";
-
-// let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJsOUdPRE13QXloUHhXZW42OHJycnVDSU9RYmYxRUx0OEpMTEg3MkFyYU0xaTdoUlVRdSIsImp0aSI6ImUyNTAzNzNmYTNkYzZlY2MzNzk3YTM2ZGYxYmVkY2Q3MjViYTY0ZTVmNGU4ZTRmMjU3NjdkZGRjOWEwNDMyMGVkZGI4ZWQ5ZjY2OTBmZWI0IiwiaWF0IjoxNjU2NTIxMDMzLCJuYmYiOjE2NTY1MjEwMzMsImV4cCI6MTY1NjUyNDYzMywic3ViIjoiIiwic2NvcGVzIjpbXX0.Az2JWFbowOkEGSo1fh2tkO3g8LTW6YIRDL23d204hRC4BXvrJLI0LiVDBnddWY8KnumrI17_AFlJ4jq4JGeFGrOmFBHEAa2dCaW1OI7WInta7QVOsaOp_JiBUKdw5-4jkB8gQhi79WnKu9qryMQ4vr3hphPuZsTNOKsrdaHxLIjNEyqcXAjN5PsiHS8S95VcJBgqxzV6bhi5ffrHQRjWP5_P39YjhBNmUcrN10ahDMYOwGyyWZ6PVSKMYFFGShcBMOZ5E16pcaxwCakqmC0It1HAmsdGCb8aq-n4B12eKq986Jj_IYlqAqAloxfwWY1FK8112PeaZYD3wNbZe-wpCw";
 export default class PetSearch {
-  static getPet() {
+  constructor() {
+    this.testObj = {};
+  }
+  static async getPet(breedName, postalCode) {
     fetch("https://api.petfinder.com/v2/oauth2/token", {
       method: "POST",
-      body: `grant_type=client_credentials&client_id=${process.env.API_KEY}&client_secret${SECRET}`,
+      body: `grant_type=client_credentials&client_id=${process.env.API_KEY}&client_secret${process.env.SECRET}`,
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
@@ -20,8 +20,10 @@ export default class PetSearch {
 
         // Return a second API call
         // This one uses the token we received for authentication
-        return fetch(
-          "https://api.petfinder.com/v2/types/dog/breeds/name/cockapoo",
+        // breed = pug
+        //
+        const response = await fetch(
+          `https://api.petfinder.com/v2/animals?breed=${breedName}&location=${postalCode}`,
           {
             headers: {
               Authorization: data.token_type + " " + data.access_token,
@@ -32,12 +34,16 @@ export default class PetSearch {
       })
       .then(function (resp) {
         // Return the API response as JSON
+        console.log(resp);
         return resp.json();
+        // return test
       })
-      .then(function (data) {
-        // Log the pet data
-        console.log("pets", data);
-      })
+      // .then(function (animals) {
+      //   // Log the pet data
+      //   console.log(animals);
+      //   // return data;
+      //   // console.log(response.animals[0].description);
+      // })
       .catch(function (err) {
         // Log any errors
         console.log("something went wrong", err);
