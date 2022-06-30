feb10a809140fc3f656bfed359a803c58cb28eda
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-30 11:32:29 -0700

    display animals info

--- src/index.js
+++ src/index.js
@@ -7,16 +7,31 @@ import PetSearch from "./js/search.js";
 function getElements(data) {
   console.log(data);
   if (data.animals) {
-    $(".output").text(`${JSON.stringify(data.animals[0].description)}`);
-    $(".output").append(`<img src="${data.animals[0].photos[1].small}"></img`);
+    displayAnimals(data);
   } else {
     console.log("no");
   }
 }
 
+function displayAnimals(data) {
+  for (let i = 0; i < data.animals.length; i++) {
+    $(".output").append(
+      `<div class="${i}"> 
+      <br>
+      <img src="${data.animals[i].photos[0].medium}"></img>
+      <br>
+      Name: ${data.animals[i].name}
+      <br>
+      Location: ${data.animals[i].contact.address.city}
+      <br>
+      Contact: ${data.animals[i].contact.email}
+      </div>`
+    );
+  }
+}
+
 async function makeApiCall(breedName, postalCode) {
   const response = await PetSearch.getPet(breedName, postalCode);
-  console.log(response);
   return response;
 }
 
@@ -25,11 +40,11 @@ $(document).ready(function () {
     event.preventDefault();
     let breedName = $("#dogs").val();
     let postalCode = $("#zip").val();
+    $(".output").children('div').remove();
     let result = makeApiCall(breedName, postalCode);
-    result.then(function(data) {
-      console.log(data);
+    result.then(function (data) {
       getElements(data);
-    })
+    });
   });
 });
 
